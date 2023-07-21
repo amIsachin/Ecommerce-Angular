38841415e22622e3c2c1c67061c547906cc1e07d
amIsachin <amIsachin@hotmail.com>
2023-07-21 23:55:43 +0530

    design home page and cards

--- src/app/src/Modules/home/Components/hero-section/hero-section.component.ts
+++ src/app/src/Modules/home/Components/hero-section/hero-section.component.ts
@@ -7,4 +7,6 @@ import { Component } from '@angular/core';
 })
 export class HeroSectionComponent {
 
+  public heroImage = '.../app/assets/assets/hero.png';
+
 }
--- src/app/src/Modules/home/home.module.ts
+++ src/app/src/Modules/home/home.module.ts
@@ -5,16 +5,23 @@ import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
 import { HeroSectionComponent } from './Components/hero-section/hero-section.component';
 
+import { MatCardModule } from '@angular/material/card';
+import { CardsComponent } from './Components/cards/cards.component';
+import { MatButtonModule } from '@angular/material/button';
+
 
 @NgModule({
   declarations: [
     HomeComponent,
-    HeroSectionComponent
+    HeroSectionComponent,
+    CardsComponent
   ],
   imports: [
     CommonModule,
-    HomeRoutingModule
+    HomeRoutingModule,
+    MatCardModule,
+    MatButtonModule
   ],
-  exports: [HeroSectionComponent]
+  exports: [HeroSectionComponent, CardsComponent]
 })
 export class HomeModule { }
